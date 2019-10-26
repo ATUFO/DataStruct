018c6a7de5ebaea81f9fdb60e3b76f2ad845307c
taofu's wsl <2221057667@qq.com>
2019-10-26 21:18:19 +0800

    bubble sort

---
+++ bubble/bubble.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int seq[1000];
+int index[1000];
+int n;
+
+void display(int d);
+
+int main()
+{
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> seq[i];
+        index[i] = i;
+    }
+
+    for (int i = 1; i < n; i++)
+    {
+        display(n - i + 1);
+        for (int j = 0; j < n - i; j++)
+        {
+            if (seq[j] > seq[j + 1])
+            {
+                swap(seq[j], seq[j + 1]);
+                swap(index[j], index[j + 1]);
+            }
+        }
+    }
+    display(n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        cout << setw(2) << index[i] << " ";
+    }
+    cout << endl;
+}
+
+void display(int d)
+{
+    for (int i = 0; i <= d; i++)
+        if (seq[i])
+            cout << setw(2) << seq[i] << " ";
+    cout << endl;
+}
---
+++ bubble/run.sh
@@ -0,0 +1,1 @@
+g++ -o bubble bubble.cpp &&cat in.txt|./bubble
