3086f252959b3790d50b3413a9a316260de2f688
taofu's wsl <2221057667@qq.com>
2019-10-26 15:55:26 +0800

    add comment to Hash.cpp

--- hash/hash.cpp
+++ hash/hash.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <cstring>
 #include <cstdlib>
-
 using namespace std;
 
 typedef struct node
@@ -10,20 +8,20 @@ typedef struct node
     node *next;
 } node;
 
-int T, N, M;   // T hashTable size,N the module of hash ,M count of input
-int *lenCount; //record the count of Length of each Link;
-node *hashTable;
+int T, N, M;     // T hashTable size,N the module of hash ,M count of input
+int *lenTable;   // 长度表，记录每一个链的长度
+node *hashTable; // 哈希表
 
-void insertToLink(node *head, int x);
-void displayLink();
-void displayASL();
+void insertToLink(node *head, int num); // 插入链表
+void displayLink();                     // 输出链表
+void displayASL();                      // 输出ASL
 
 int main()
 {
     int x; // temp var
     cin >> T >> N >> M;
-    hashTable = (node *)malloc(sizeof(node) * T);
-    lenCount = (int *)malloc(sizeof(int) * T);
+    hashTable = (node *)malloc(sizeof(node) * T); // 初始化hash表
+    lenTable = (int *)malloc(sizeof(int) * T);    // 初始化长度表
     for (int i = 0; i < M; i++)
     {
         cin >> x;
@@ -33,16 +31,19 @@ int main()
     displayASL();
 }
 
-void insertToLink(node *head, int x)
+void insertToLink(node *head, int num)
 {
     node *ptr = head;
-    ptr->x++;
-    lenCount[ptr->x]++;
-    node *newNode = (node *)malloc(sizeof(node));
-    newNode->x = x;
-    while (ptr->next)
+
+    ptr->x++; // 增长链表长度并记录
+    lenTable[ptr->x]++;
+
+    node *newNode = (node *)malloc(sizeof(node)); // 生成新节点
+    newNode->x = num;
+
+    while (ptr->next) // 如果待插入节点比ptr->next->x 小，那么插入。否则在56行插入至尾部
     {
-        if (x < ptr->next->x)
+        if (num < ptr->next->x)
         {
             newNode->next = ptr->next;
             ptr->next = newNode;
@@ -50,16 +51,13 @@ void insertToLink(node *head, int x)
         }
         ptr = ptr->next;
     }
-    if (ptr->next == NULL)
-    {
-        newNode->next = ptr->next;
-        ptr->next = newNode;
-    }
+    newNode->next = ptr->next;
+    ptr->next = newNode;
 }
 
 void displayLink()
 {
-    for (int i = 0; i < T; i++)
+    for (int i = 0; i < T; i++) // 遍历每一条链
     {
         node *cur = hashTable + i;
         cout << i;
@@ -73,16 +71,18 @@ void displayLink()
     }
 }
 
-void displayASL()
+void displayASL() // 计算及输出ASL ，控制格式稍微麻烦一点
 {
     cout << "ASL="
          << "(";
     int sum = 0;
     for (int i = 1; i < T; i++)
     {
-        sum += lenCount[i] * i;
-        if (lenCount[i])
-            cout << i << (lenCount[i] > 1 ? ("*" + to_string(lenCount[i])) : "") << "+";
+        sum += lenTable[i] * i;
+        if (lenTable[i])
+            cout << i
+                 << (lenTable[i] > 1 ? ("*" + to_string(lenTable[i])) : "")
+                 << "+";
     }
     cout << "\b"
          << ")/" << M << "=" << (1.0 * sum / M) << endl;
